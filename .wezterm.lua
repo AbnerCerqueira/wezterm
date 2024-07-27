b984114d91cc95b150da0a781f9ecff8c441ab8c
AbnerCerqueira <abner.cerqueira@fatec.sp.gov.br>
2024-07-26 21:31:26 -0300

    :sparkles: Fonte e opacidade diferentes

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,10 +3,10 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.window_decorations = "NONE"
-config.window_background_opacity = 0.95
+config.window_background_opacity = 0.99
 
 config.font = wezterm.font 'Jetbrains Mono'
 
-config.color_scheme = 'Catppuccin Mocha'
+config.color_scheme = 'Breeze'
 
 return config
