106226353a62b9ea02a61483b33b786373410e08
AbnerCerqueira <abner.cerqueira@fatec.sp.gov.br>
2024-07-30 20:59:48 -0300

    :sparkles: CloseCurrentPane

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,11 +2,19 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.window_decorations = "NONE"
+config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.99
 
 config.font = wezterm.font 'Jetbrains Mono'
 
 config.color_scheme = 'Breeze'
+
+config.keys = {
+    {
+        key = 'W',
+        mods = 'CTRL|SHIFT|ALT',
+        action = wezterm.action.CloseCurrentPane { confirm = true },
+    },
+}
 
 return config
