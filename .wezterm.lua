3bfe2c615a282829dfdd9148ab9a02ad98e6f012
Abner Cerqueira <abner.cerqueira@fatec.sp.gov.br>
2024-11-25 23:36:47 -0300

    :lipstick: Update theme

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,11 +3,11 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 1
+config.window_background_opacity = 0.98
 
 config.font = wezterm.font 'Jetbrains Mono'
 
-config.color_scheme = 'Tokyo Night'
+config.color_scheme = 'Default (dark) (terminal.sexy)'
 
 config.keys = {
     {
